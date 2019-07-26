b6b9d558e28421db877f5d41feb6b36027502e35
Steve Barrett <steven122185@gmail.com>
2019-07-26 11:31:19 -0400

    Imported the starting project as well as setup the model for Authors and Book

---
+++ StartHere/src/main/java/com/lambdaschool/starthere/models/Authors.java
@@ -0,0 +1,80 @@
+package com.lambdaschool.starthere.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@ApiModel(value = "Course", description =  "All the courses students can take")
+@Entity
+@Table(name = "authors")
+public class Authors
+{
+    @ApiModelProperty(name = "authorid", value = "primary key for author", required = true, example = "1")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long authorid;
+
+    @ApiModelProperty(name = "lastname", value = "Author last name", required = true, example = "King")
+    private String lastname;
+
+    @ApiModelProperty(name = "firstname", value = "Author first name", required = true, example = "Stephen")
+    private String firstname;
+
+    @ManyToMany(mappedBy = "book")
+    @JsonIgnoreProperties("authors")
+    private List<Book> books = new ArrayList<>();
+
+    public Authors()
+    {
+    }
+
+    public Authors(String lastname, String firstname)
+    {
+        this.lastname = lastname;
+        this.firstname = firstname;
+    }
+
+    public long getAuthorid()
+    {
+        return authorid;
+    }
+
+    public void setAuthorid(long authorid)
+    {
+        this.authorid = authorid;
+    }
+
+    public String getLastname()
+    {
+        return lastname;
+    }
+
+    public void setLastname(String lastname)
+    {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname()
+    {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname)
+    {
+        this.firstname = firstname;
+    }
+
+    public List<Book> getBooks()
+    {
+        return books;
+    }
+
+    public void setBooks(List<Book> books)
+    {
+        this.books = books;
+    }
+}
---
+++ StartHere/src/main/java/com/lambdaschool/starthere/models/Book.java
@@ -0,0 +1,97 @@
+package com.lambdaschool.starthere.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@ApiModel(value = "Course", description =  "All the courses students can take")
+@Entity
+@Table(name = "book")
+public class Book
+{
+    @ApiModelProperty(name = "bookid", value = "primary key for book", required = true, example = "1")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long bookid;
+
+    @ApiModelProperty(name = "booktitle", value = "Book Title", required = true, example = "Physics")
+    private String booktitle;
+
+    @ApiModelProperty(name = "ISBN", value = "International Standard Book Number(ISBN) of the book", required = true, example = "1984036782")
+    private String ISBN;
+
+    @ApiModelProperty(name = "copy", value = "copywrite - the year the book was published", required = true, example = "1978")
+    private int copy;
+
+    @ManyToMany
+    @JoinTable(name = "bookauthors",
+            joinColumns = {@JoinColumn(name = "bookid")},
+            inverseJoinColumns = {@JoinColumn(name = "authorid")})
+    @JsonIgnoreProperties("books")
+    private List<Authors> authors = new ArrayList<>();
+
+    public Book()
+    {
+    }
+
+    public Book(String booktitle, String ISBN, int copy)
+    {
+        this.booktitle = booktitle;
+        this.ISBN = ISBN;
+        this.copy = copy;
+    }
+
+    public long getBookid()
+    {
+        return bookid;
+    }
+
+    public void setBookid(long bookid)
+    {
+        this.bookid = bookid;
+    }
+
+    public String getBooktitle()
+    {
+        return booktitle;
+    }
+
+    public void setBooktitle(String booktitle)
+    {
+        this.booktitle = booktitle;
+    }
+
+    public String getISBN()
+    {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN)
+    {
+        this.ISBN = ISBN;
+    }
+
+    public int getCopy()
+    {
+        return copy;
+    }
+
+    public void setCopy(int copy)
+    {
+        this.copy = copy;
+    }
+
+    public List<Authors> getAuthors()
+    {
+        return authors;
+    }
+
+    public void setAuthors(List<Authors> authors)
+    {
+        this.authors = authors;
+    }
+}
